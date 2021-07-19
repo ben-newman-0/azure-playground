b1975ae81e28ede3a1f2c8bd054347d2e0e9f1f5
Ben Newman <ben@newman.ws>
2021-07-19 21:29:52 +0100

    Inline config into script and add CustomScript extension

--- resourceGroups/nethub/dns-fwd/init.sh
+++ resourceGroups/nethub/dns-fwd/init.sh
@@ -1,2 +1,21 @@
-sudo apt update
-sudo apt install bind9
+apt-get update && apt-get upgrade -y
+apt-get install -y bind9
+
+cat > /etc/bind/named.conf.options << EOF
+options {
+  directory "/var/cache/bind";
+
+  recursion yes;
+  allow-query {
+    10.0.0.0/8;
+    172.16.0.0/12;
+    192.168.0.0/16;
+    127.0.0.1;
+  };
+
+  forwarders { 168.63.129.16; };
+  forward only;
+};
+EOF
+
+service bind9 restart
